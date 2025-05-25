c8dd511b899e49db1d86851a8969f68d943feff6
vornodom <vornodom01@gmail.com>
2025-05-25 14:24:20 +0700

    add meta

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://metadata-git-main-vornodoms-projects.vercel.app',
+  }
+}
